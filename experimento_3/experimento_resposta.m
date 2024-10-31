6343b3adccad850d2f98475d12c75555be04a3ad
luccacastro <luccacastro1008@poli.ufrj.br>
2024-10-31 14:09:35 -0300

    Adicionando validação da resposta em frequencia

--- experimento_3/experimento_resposta.m
+++ experimento_3/experimento_resposta.m
@@ -15,15 +15,15 @@ for i = 1:10
     T = 1/f_k; % Período da frequência
     
     % Ajuste de tempo, começando de zero
-    k = (data.('inS') - data.('inS')(1));  % Subtrai o valor inicial do ponto de índice 1800
+    k = (data.('inS') - data.('inS')(1800));  % Subtrai o valor inicial do ponto de índice 1800
     
     % Filtrar os valores de tempo que são menores ou iguais a T, a partir do índice 1800
-    t = k(1:end);  % Começa do índice 1800 até o final
+    t = k(1800:end);  % Começa do índice 1800 até o final
     t = t(t <= T);  % Filtra para pegar apenas um período
     
     % Filtrar os sinais de entrada e saída com base no tamanho do vetor de tempo
-    saida = data.('C2InV')(1:1+length(t)-1);  % Ajusta o sinal de saída para o mesmo tamanho do tempo filtrado
-    entrada = data.('C1InV')(1:1+length(t)-1);  % Ajusta o sinal de entrada para o mesmo tamanho do tempo filtrado
+    saida = data.('C2InV')(1800:1800+length(t)-1);  % Ajusta o sinal de saída para o mesmo tamanho do tempo filtrado
+    entrada = data.('C1InV')(1800:1800+length(t)-1);  % Ajusta o sinal de entrada para o mesmo tamanho do tempo filtrado
 
     
     % Cálculo para a entrada
@@ -53,6 +53,8 @@ for i = 1:10
     end
 end
 
+freqs_wn = 2 * pi * freqs;
+
 % Plotar o diagrama de Bode
 figure;
 subplot(2, 1, 1);
@@ -69,29 +71,45 @@ ylabel('Defasagem (graus)');
 title('Diagrama de Bode - Fase');
 grid on;
 
-% ##### Método dos mínimos quadrados (3 primeiros pontos) #############
+% ##### Método dos mínimos quadrados (3 primeiros pontos) ########
 figure
 
 first_freqs_points = freqs(1,1:3);
 first_gjwdb_points = gjwdb(1,1:3);
 
-[coeffs, adjusted_gjwdb_points] = minimos_quadrados(first_freqs_points, first_gjwdb_points, 1);
+[coeffs, adjusted_gjwdb_points] = minimos_quadrados(first_freqs_points, first_gjwdb_points, 8);
 
-plot(first_freqs_points, [first_gjwdb_points; adjusted_gjwdb_points], '-o'); 
-
-xlabel('w');
-ylabel('db');
-
-title('Ajuste por aproximação linear de gjwdb');
-
-grid on;
 
 % ##### Método dos mínimos quadrados (todos os pontos) #############
-figure
 
-[coeffs, adjusted_gjwdb_points] = minimos_quadrados(freqs, gjwdb, 3);
+[coeffs, adjusted_gjwdb_points] = minimos_quadrados(freqs, gjwdb, 8);
 
-plot(freqs, [gjwdb; adjusted_gjwdb_points], '-o'); 
+ww = logspace(log10(0.189), log10(18.9), 200);
+
+g = polyval(coeffs, ww);
+
+% deslocando a curva para passar na origem
+    y_zero = adjusted_gjwdb_points(1);
+    for i = 1:size(g, 2)
+        g(i) = g(i) + y_zero;
+    end
+
+KdB = mean(adjusted_gjwdb_points(1,1:2));
+
+semilogx(freqs_wn, [gjwdb; adjusted_gjwdb_points], '-o'); 
+hold on
+% semilogx(ww*2*pi, g, '--');
+
+yline(KdB, '--r', sprintf('x = %.2f', KdB));
+yline((KdB - 3), '--r', sprintf('x = %.2f', KdB - 3));
+% xline ((8.82), '--r', 'y = 8.82');
+% xline ((1.4037), '--r', 'y = 1.4037');
+
+K = 10^(KdB/20)
+
+[~, idx] = min(abs(adjusted_gjwdb_points - (KdB-3)));
+freq_corte = freqs_wn(idx);
+tau = 1/freq_corte
 
 xlabel('w');
 ylabel('db');
@@ -99,3 +117,43 @@ ylabel('db');
 title('Ajuste por aproximação linear de gjwdb');
 
 grid on;
+hold on
+
+
+% Criando a função de transferência
+num = K;
+den = [tau 1];
+sys = tf(num, den);
+
+% Gerando o Bode plot
+[mag, phase, w] = bode(sys, {freqs_wn(1), freqs_wn(10)});
+mag_db = 20 * log10(squeeze(mag));
+freq_hz = squeeze(w) / (2 * pi);
+phase = squeeze(phase);
+
+% Figura para o Bode Plot Modificado
+figure;
+
+% Primeiro subplot - Magnitude (Ganho em dB)
+subplot(2, 1, 1);
+semilogx(freq_hz, mag_db, '-');       
+hold on;
+semilogx(freqs, gjwdb, 'o');          
+hold off;
+grid on;
+title('Diagrama de Bode - Ganho');
+xlabel('Frequência (Hz)');
+ylabel('Magnitude / Ganho (dB)');
+legend('Magnitude (dB)', 'Ganho (dB)', 'Location', 'best');
+
+% Segundo subplot - Fase
+subplot(2, 1, 2);
+semilogx(freq_hz, phase, '-');        
+hold on;
+semilogx(freqs, phi, 'o');           
+hold off;
+grid on;
+title('Diagrama de Bode - Fase');
+xlabel('Frequência (Hz)');
+ylabel('Fase (graus)');
+legend('Fase (graus)', 'Defasagem (graus)', 'Location', 'best');
